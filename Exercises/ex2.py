7851a8d6d5818df3ec38d72d128386e5e46a93db
Evan <evan@evangriffith.net>
2015-01-25 01:04:12 -0800

    Finished night of 1/25

---
+++ Exercises/ex2.py
@@ -0,0 +1,8 @@
+# A comment
+
+print "I could have the code like this." # and this is ignored
+
+# Disable a line with the octothorpe
+# print "Not working."
+
+print "This will work."
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # LearnPythonTheHardWay
 Exercises in Learn Python the Hard Way
+
+http://learnpythonthehardway.org/book/ex6.html
