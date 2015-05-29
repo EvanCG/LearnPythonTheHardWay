54e2b9cc44de126d8d29a255fe60fbb995f7f6c1
Evan Griffith <Evan@EvanGriffith.net>
2015-05-28 23:45:45 -0700

    Improved the last string print statement

--- Exercises/ex13.py
+++ Exercises/ex13.py
@@ -9,4 +9,4 @@ print "Your third variable is:", third
 
 thoughts = raw_input("What do you think of this program? ")
 
-print "Interesting, you think '", thoughts, "'. That's interesting!"
+print "Interesting, you think '%s'. That's interesting!" % thoughts
